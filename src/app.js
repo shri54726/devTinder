736eb9f100eeb48c560278cd24bc78c82b03a2ce
Shri <shri.korat@gmail.com>
2025-02-21 00:01:06 +0530

    api routs

--- src/app.js
+++ src/app.js
@@ -4,13 +4,23 @@ const app = express();
 
 const port = 3000;
 
+app.get('/user',(req, res)=>{
+    res.send({name: "Shrikant", email: "shri@123.com"})
+})
+
+app.post('/user',(req, res)=>{
+    res.send("Data saved successfully")
+})
+
+app.delete('/user',(req, res)=>{
+    res.send("Data deleted successfully")
+})
+
 app.use('/',(req, res)=>{
     res.send('Hellow from dashboard');
 });
 
-app.use('/test',(req, res)=>{
-    res.send('Hellow World');
-});
+
 
 app.listen(port, ()=>{
     console.log(`Server is listing to ${port}`);
