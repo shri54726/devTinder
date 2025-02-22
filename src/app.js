68e9f6623d884b669bd4c3ccb387b4f6d1aa5985
Shri <shri.korat@gmail.com>
2025-02-22 19:40:55 +0530

    getting all the users

--- src/app.js
+++ src/app.js
@@ -2,19 +2,15 @@ const express = require('express')
 const connectDB = require('./config/database')
 const app = express();
 const User = require('./models/user')
+app.use(express.json())
 
 const port = 3000;
 
 app.post('/signup', async (req,res)=>{
     try {
-        const userObj = {
-            firstName: 'Virat',
-            lastName: 'Kohli',
-            emailId: 'virat@gmail.com',
-            password: '123456',
-        }
-    
-        const user = new User(userObj)
+        const userObj = req.body;
+
+        const user = new User(userObj);
     
         await user.save();
     
@@ -22,7 +18,41 @@ app.post('/signup', async (req,res)=>{
     } catch (error) {
         res.status(400).send("Error while saving "+ error )
     }
-})
+});
+
+app.get('/getUserByEmail', async (req, res)=>{
+    const userEmail = req.query.email;
+    try {
+        const user = await User.findOne({emailId: userEmail}).exec();
+        if(!user){
+            res.status(404).send('user not found')
+        }else{
+            res.send(user)
+        }
+        // const user = await User.find({emailId: userEmail});
+        // if(user.length === 0){
+        //     res.status(404).send('user not found')
+        // }else{
+        //     res.send(user)
+        // }
+    } catch (error) {
+        res.status(400).send("Error while getting user data "+ error )
+    }
+});
+
+
+app.get('/getAllUser', async (req, res)=>{
+    try {
+        const user = await User.find({});
+        if(user.length === 0){
+            res.status(404).send('users not found')
+        }else{
+            res.send(user)
+        }
+    } catch (error) {
+        res.status(400).send("Error while getting user data "+ error )
+    }
+});
 
 
 connectDB().then(()=>{
