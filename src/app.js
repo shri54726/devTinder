423600f4e2bf73983fd18b3ec46d0c6986fcd0a8
Shri <shri.korat@gmail.com>
2025-02-22 23:45:48 +0530

    Model and validations

--- src/app.js
+++ src/app.js
@@ -54,6 +54,42 @@ app.get('/getAllUser', async (req, res)=>{
     }
 });
 
+app.delete('/deleteUserById', async (req, res)=>{
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete({_id: userId});
+        res.send('user deleted')
+    } catch (error) {
+        res.status(400).send("user not deleted")
+    }
+
+});
+
+
+app.patch('/updateUserById', async (req, res)=>{
+    const userId = req.query.userId;
+    const data = req.body;
+    try {
+        const AllowedKeys = ['userId','gender','skills','photoUrl']
+
+        const isUpdateAllowed = Object.keys(data).every((k)=>AllowedKeys.includes(k));
+
+        if(!isUpdateAllowed){
+            throw new Error('Update is not allowed');
+        }
+
+        if(data.skills.length>10){
+            throw new Error('Skills should not exceed 10');
+        }
+
+        const user = await User.findByIdAndUpdate({_id:userId}, data, {returnDocument: 'after', runValidators: true});
+        res.send(user)
+    } catch (error) {
+        res.status(400).send("user not updated " + error)
+    }
+
+});
+
 
 connectDB().then(()=>{
     console.log('Database Connection Successful...')
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,65 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 const UserSchema = new mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        required: true
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('This Email is not Valid => '+value)
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        minLength: 8,
+        required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error('Please enter strong password => '+value);
+            }
+        }
     },
     age:{
-        type:String
+        type:String,
+        min: 18
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!['male','female','others'].includes(value)){
+                throw new Error('gender data is invalid');
+            }
+        }
     },
-});
+    photoUrl:{
+        type: String,
+        default: "https://www.shutterstock.com/image-vector/vector-design-avatar-dummy-sign-600nw-1290556063.jpg",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error('Invalid url => '+value);
+            }
+        }
+    },
+    about:{
+        type: String,
+        default:'Default value of user'
+    },
+    skills:{
+        type:[String]
+    }
+},{timestamps:true});
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
