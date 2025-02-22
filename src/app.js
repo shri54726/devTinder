27bf75bb529220071979276e104ba0f8a91aa6f6
Shri <shri.korat@gmail.com>
2025-02-22 18:38:25 +0530

    BD

--- src/app.js
+++ src/app.js
@@ -1,84 +1,35 @@
 const express = require('express')
-
+const connectDB = require('./config/database')
 const app = express();
+const User = require('./models/user')
 
 const port = 3000;
 
-const { userAuth, adminAuth } = require('./middleware/auth')
-
-// app.get('/user',(req, res)=>{
-//     res.send({name: "Shrikant", email: "shri@123.com"})
-// })
-
-// app.post('/user',(req, res)=>{
-//     res.send("Data saved successfully")
-// })
-
-// app.delete('/user',(req, res)=>{
-//     res.send("Data deleted successfully")
-// })
-
-// app.use('/',(req, res)=>{
-//     res.send('Hellow from dashboard');
-// });
-
-// app.use('/',(req, res,next)=>{
-//     console.log('Hello from dashboard');
-//     next();
-// })
-
-// app.get('/user',(req, res, next)=>{
-//         console.log('Hello from user route1')
-//         next();
-//     },
-//     (req,res)=>{
-//         res.send({name: "Shrikant", email: "shri@123.com"})
-//     },
-//     (req,res)=>{
-//         res.send({name: "Shrikant", email: "shri@123.com"})
-//     }
-// );
-
-
-
-// app.use('/admin', adminAuth);
-
-app.use('/',(err, req, res, next)=>{
-    if(err){
-        res.status(500).send('Something Went Wrong')
-    }
-})
-
-app.get('/admin/getAdminData',(req, res)=>{
+app.post('/signup', async (req,res)=>{
     try {
-        throw new Error("abovs")
+        const userObj = {
+            firstName: 'Virat',
+            lastName: 'Kohli',
+            emailId: 'virat@gmail.com',
+            password: '123456',
+        }
+    
+        const user = new User(userObj)
+    
+        await user.save();
+    
+        res.send('User Added Successfully');
     } catch (error) {
-        res.status(500).send('went wrong')
+        res.status(400).send("Error while saving "+ error )
     }
 })
 
-app.get('/user/getUserData',(req, res)=>{
-    res.send('All user data sent');
-})
 
-app.post('/user/login',(err, req,res, next)=>{
-    // try {
-        // res.send('Login Successfull')
-        throw new Error("abovs")
-    // } catch (err) {
-    //     res.status(500).send('Login Unsfccessful')
-    // }
+connectDB().then(()=>{
+    console.log('Database Connection Successful...')
+    app.listen(port, ()=>{
+        console.log(`Server is listing to ${port}`);
+    });
+}).catch(err=>{
+    console.log('Database Ccnnection Unsuccessful..')
 })
-app.use('/',(err, req, res, next)=>{
-    if(err){
-        res.status(500).send('Something Went Wrong')
-    }
-})
-
-app.delete('/admin/deleteData',(req,res)=>{
-    res.send('Data deleted')
-})
-
-app.listen(port, ()=>{
-    console.log(`Server is listing to ${port}`);
-});
