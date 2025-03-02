7a08f3e399990bf38a0f5fe6a8d79ceafe3953ca
Shri <shri.korat@gmail.com>
2025-03-02 21:08:00 +0530

    DB changes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
     mongoose.connect(
-        "mongodb://0.0.0.0:27017/devTinder"
+        "mongodb+srv://dev-tinder-user:QgPLeSYCVFlHSUHz@cluster0.z54it.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
 }
 
